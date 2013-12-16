9ee930d5eae03bd89ce608f37e81f8ea22736ace
Ryan Erdmann <ryanerdmann@hotmail.com>
2013-12-16 13:05:17 -0600

    Potentially finished the ALU

--- processor/ALU/ALU.vhd
+++ processor/ALU/ALU.vhd
@@ -12,8 +12,9 @@ entity ALU is
 	port(
 		inputOne, inputTwo : in std_logic_vector(7 downto 0);
 		aluCode : in std_logic_vector(2 downto 0);
+		clk_in : std_logic;
 		output : out std_logic_vector(7 downto 0);
-		zero, overflow, carry : out bit
+		zero, overflow : out bit
 	);
 end ALU;
 
@@ -29,28 +30,54 @@ end ALU;
 
 architecture architectureALU of ALU is
 begin
-	process(aluCode)
+	process(clk_in)
+		variable extendedOne, extendedTwo, overflowSum : std_logic_vector(15 downto 0);
 	begin
-		if aluCode = "000" then
-			-- and
-			output <= std_logic_vector(inputOne and inputTwo);
-			zero <= '0';
-			overflow <= '0';
-			carry <= '0';
-		elsif aluCode = "001" then
-			-- or
-		elsif aluCode = "010" then
-			-- add
-		elsif aluCode = "011" then
-			-- sll
-		elsif aluCode = "100" then
-			-- slt
-		elsif aluCode = "101" then
-			-- xor
-		elsif aluCode = "110" then
-			-- sub
-		elsif aluCode = "111" then
-			-- srl
+		if (rising_edge(clk_in)) then
+			if aluCode = "000" then
+				-- and
+				output <= inputOne and inputTwo;
+			elsif aluCode = "001" then
+				-- or
+				output <= inputOne or inputTwo;
+			elsif aluCode = "010" then
+				-- add
+				extendedOne := "00000000" & inputOne;
+				extendedTwo := "00000000" & inputTwo;
+				overflowSum := std_logic_vector(unsigned(extendedOne) + unsigned(extendedTwo));
+				
+				if (to_integer(unsigned(overflowSum(15 downto 8))) = 0) then
+					overflow <= '0';
+				else
+					overflow <= '1';
+				end if;				
+				
+				output <= std_logic_vector(unsigned(inputOne) + unsigned(inputTwo));
+			elsif aluCode = "011" then
+				-- sll
+				output <= std_logic_vector(unsigned(inputOne) sll to_integer(unsigned(inputTwo)));
+			elsif aluCode = "100" then
+				-- slt
+				if (to_integer( unsigned(inputOne) - unsigned(inputTwo)) > 0) then
+					output <= "00000001";
+				else
+					output <= "00000000";
+				end if;
+			elsif aluCode = "101" then
+				-- xor
+				output <= inputOne xor inputTwo;		
+			elsif aluCode = "110" then
+				-- sub
+				output <= std_logic_vector(signed(inputOne) - signed(inputTwo));
+				if (to_integer(signed(inputOne) - signed(inputTwo)) = 0) then
+					zero <= '1';
+				else 
+					zero <= '0';
+				end if;
+			elsif aluCode = "111" then
+				-- srl
+				output <= std_logic_vector(unsigned(inputOne) srl to_integer(unsigned(inputTwo)));
+			end if;
 		end if;
 	end process;
 end architectureALU;
