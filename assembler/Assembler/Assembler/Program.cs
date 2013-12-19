ac7d97bc61a7516af73bb511a1b5cbfa9afeb7ad
Ryan Erdmann <ryanerdmann@hotmail.com>
2013-12-19 00:19:07 -0600

    Assembler improvements and some sample files

--- assembler/Assembler/Assembler/Program.cs
+++ assembler/Assembler/Assembler/Program.cs
@@ -13,13 +13,14 @@ namespace Assembler
         {
             Console.WriteLine("Enter base repository directory:  ex: C:/git");
             var basePath = Console.ReadLine();
+            var program = "multiplier";
 
-            var inputPath = String.Format("{0}/processor/processor/FinalProcessor/program.s", basePath);
+            var inputPath = String.Format("{0}/processor/processor/FinalProcessor/{1}.s", basePath, program);
             var lines = File.ReadAllLines(inputPath);
             Console.WriteLine("Assembly file read from {0}", inputPath);
             var assembler = new Assembler(lines);
             var mif = assembler.GetMachineCode();
-            var outputPath = String.Format("{0}/processor/processor/FinalProcessor/program.mif", basePath);
+            var outputPath = String.Format("{0}/processor/processor/FinalProcessor/{1}.mif", basePath, program);
             File.WriteAllLines(outputPath, mif);
             Console.WriteLine("MIF file saved to {0}", outputPath);
 
